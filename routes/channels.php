a82a657cac7a7e57bbb1da28ec41765640066011
jss-moeka-ito <jss-moeka-ito@denkei.jp>
2020-04-27 14:22:22 +0900

    roomに自信のuser_idが存在する場合broadcastを受け取れるように修正

--- routes/channels.php
+++ routes/channels.php
@@ -11,6 +11,13 @@
 |
 */
 
-Broadcast::channel('App.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('post-comment-channel.{room_id}', function ($user, $room_id) {
+    $room = App\Room::where('id', $room_id)->first();
+
+    if((int )$user->id === (int )$room->user_1_id) {
+        return true;
+    }elseif ((int )$user->id === (int )$room->user_2_id) {
+        return true;
+    }
+    return false;
 });
